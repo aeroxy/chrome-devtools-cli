6b77034dc27382e3579f281329364333497b5dfc
Aero <aero.windwalker@gmail.com>
2026-05-16 13:33:10 +0800

    fix: handle errors when opening log file in TelemetryWorker

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -29,13 +29,19 @@ impl TelemetryWorker {
 
             while let Ok(entry) = receiver.recv() {
                 if current_path.as_deref() != Some(entry.path.as_path()) {
-                    if let Ok(new_file) = OpenOptions::new()
+                    match OpenOptions::new()
                         .create(true)
                         .append(true)
                         .open(&entry.path)
                     {
-                        current_path = Some(entry.path);
-                        current_file = Some(new_file);
+                        Ok(new_file) => {
+                            current_path = Some(entry.path);
+                            current_file = Some(new_file);
+                        }
+                        Err(_) => {
+                            current_path = None;
+                            current_file = None;
+                        }
                     }
                 }
                 if let Some(file) = current_file.as_mut() {
